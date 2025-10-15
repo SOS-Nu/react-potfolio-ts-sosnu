78047948ea4db9e34d1a55eba0cb123f6af924ea
nhokanhanh@gmail.com <nhokanhanh@gmail.com>
2025-10-15 18:17:22 +0700

    add project 10/15/2025

--- src/helpers/data.ts
+++ src/helpers/data.ts
@@ -1,147 +1,149 @@
-import nestImg from 'assets/project/nestjs.png';
-import nextjsImg from 'assets/project/nextjs.jpeg';
-import reactNativeImg from 'assets/project/react-native.png';
-import reactUltimateImg from 'assets/project/react-ultimate.png';
-import springMvcImg from 'assets/project/spring-mvc.png';
-import springRestImg from 'assets/project/spring-rest.png';
+import nestImg from "assets/project/nestjs.png";
+import nextjsImg from "assets/project/nextjs.jpeg";
+import reactNativeImg from "assets/project/react-native.png";
+import reactUltimateImg from "assets/project/react-ultimate.png";
+import springMvcImg from "assets/project/spring-mvc.png";
+import springRestImg from "assets/project/spring-rest.png";
 
 export const APP_DATA = {
-    YOUTUBE_URL: "https://www.youtube.com/@SOSNu",
-    TIKTOK_URL: "https://www.tiktok.com/@sosnu",
-    UDEMY_URL: "",
-    FACEBOOK_URL: "https://www.facebook.com/SOS.Nuu/"
-}
+  YOUTUBE_URL: "https://www.youtube.com/@SOSNu",
+  TIKTOK_URL: "https://www.tiktok.com/@sosnu",
+  UDEMY_URL: "",
+  FACEBOOK_URL: "https://www.facebook.com/SOS.Nuu/",
+};
 
 export const EXPERIENCES = [
-    {
-        id: 1,
-        title: {
-            vi: "Lập Trình Viên",
-            en: "Fullstack Developer"
-        },
-        company: {
-            vi: "Công ty ABCXYZ Ltd.",
-            en: "ABCXYZ Ltd."
-        },
-        duration: {
-            vi: "Jan 2024 - Hiện tại",
-            en: "Jan 2024 - Present"
-        }
+  {
+    id: 1,
+    title: {
+      vi: "Lập Trình Viên",
+      en: "Fullstack Developer",
     },
-    {
-        id: 2,
-        title: {
-            vi: "Lập Trình Viên",
-            en: "Fullstack Developer"
-        },
+    company: {
+      vi: "Công ty ABCXYZ Ltd.",
+      en: "ABCXYZ Ltd.",
+    },
+    duration: {
+      vi: "Jan 2024 - Hiện tại",
+      en: "Jan 2024 - Present",
+    },
+  },
+  {
+    id: 2,
+    title: {
+      vi: "Lập Trình Viên",
+      en: "Fullstack Developer",
+    },
 
-        company: {
-            vi: "Công ty F (freelancer)",
-            en: "F (freelancer)"
-        },
-        duration: {
-            vi: "Tháng 6/2022 - Tháng 1/2024",
-            en: "Jun 2022 - Jan 2024"
-        }
+    company: {
+      vi: "Công ty F (freelancer)",
+      en: "F (freelancer)",
     },
-    {
-        id: 3,
-        title: {
-            vi: "Dự án tự làm",
-            en: "Self Employed"
-        },
-        company: {
-            vi: "Học kiến thức mới mỗi ngày",
-            en: "Code and build something in everyday."
-        },
-        duration: {
-            vi: "Tháng 1/2020 - Hiện tại",
-            en: "Jan 2020 - Present"
-        }
-    }
-]
+    duration: {
+      vi: "Tháng 6/2022 - Tháng 1/2024",
+      en: "Jun 2022 - Jan 2024",
+    },
+  },
+  {
+    id: 3,
+    title: {
+      vi: "Dự án tự làm",
+      en: "Self Employed",
+    },
+    company: {
+      vi: "Học kiến thức mới mỗi ngày",
+      en: "Code and build something in everyday.",
+    },
+    duration: {
+      vi: "Tháng 1/2020 - Hiện tại",
+      en: "Jan 2020 - Present",
+    },
+  },
+];
+export const PROJECTSFeatured = [
+  {
+    id: 1,
+    imgPath: nestImg,
+    title: "JobHunter | Full-Stack Application (Featured & Latest)",
+    // SỬA Ở ĐÂY: Thay thế bằng dấu ` và xóa các dấu +
+    description: `Một nền tảng tuyển dụng, kết nối nhà tuyển dụng và ứng viên.
+- Backend được xây dựng với Java Spring (RESTful API), Hibernate, MySQL và áp dụng các Design Pattern, MVC.
+- Bảo mật bằng Spring Security với JWT, Redis cache. Tích hợp Kafka để gửi email thông báo công việc mới.
+- Call API Google Gemini, thanh toán VNPay, đăng nhập với Google, Cronjob để đối soát dữ liệu.
+- Frontend được phát triển bằng React và TypeScript, sử dụng Ant Design cho UI và Axios để gọi API.
+- Về DevOps, đóng gói bằng Docker, tự động backup database, và giám sát bằng bộ công cụ Grafana, Prometheus, Node Exporter.`,
+    githubLink: "https://github.com/your-username/jobhunter-backend",
+    demoLink: "https://your-demo-link.com",
+  },
+];
 
 export const PROJECTS = [
-    {
-        id: 1,
-        imgPath: reactNativeImg,
-        title: "React Native Basic",
-        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-        githubLink: "https://hoidanit.vn/khoa-hoc/react-native-ultimate-phat-trien-ung-dung-mobile-66e7ab4578aaf477418cb538.html",
-        demoLink: "https://hoidanit.vn/khoa-hoc/react-native-ultimate-phat-trien-ung-dung-mobile-66e7ab4578aaf477418cb538.html"
-    },
-    {
-        id: 2,
-        imgPath: springRestImg,
-        title: "Java Spring REST",
-        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-        githubLink: "https://hoidanit.vn/khoa-hoc/java-spring-rest-ultimate-for-beginners-6631cb577353ab6861b188f6.html",
-        demoLink: "https://hoidanit.vn/khoa-hoc/java-spring-rest-ultimate-for-beginners-6631cb577353ab6861b188f6.html"
-    },
-    {
-        id: 3,
-        imgPath: springMvcImg,
-        title: "Java Spring MVC",
-        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-        githubLink: "https://hoidanit.vn/khoa-hoc/java-spring-mvc-ultimate-for-beginners-65ce0b770c05f4450fbd86ac.html",
-        demoLink: "https://hoidanit.vn/khoa-hoc/java-spring-mvc-ultimate-for-beginners-65ce0b770c05f4450fbd86ac.html"
-    },
-    {
-        id: 4,
-        imgPath: nextjsImg,
-        title: "React ProMax with Next.js",
-        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-        githubLink: "https://hoidanit.vn/khoa-hoc/react-pro-max-voi-nextjs-lam-chu-toan-dien-reactjs-hien-dai-65198100e6bafa8caad417a6.html",
-        demoLink: "https://hoidanit.vn/khoa-hoc/react-pro-max-voi-nextjs-lam-chu-toan-dien-reactjs-hien-dai-65198100e6bafa8caad417a6.html"
-    },
-    {
-        id: 5,
-        imgPath: nestImg,
-        title: "Nest.js Basic",
-        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-        githubLink: "https://hoidanit.vn/khoa-hoc/nestjs-voi-typescript-mongodb-sieu-de-64686ec6fb456bbb90663dd6.html",
-        demoLink: "https://hoidanit.vn/khoa-hoc/nestjs-voi-typescript-mongodb-sieu-de-64686ec6fb456bbb90663dd6.html"
-    },
-    {
-        id: 6,
-        imgPath: reactUltimateImg,
-        title: "React Ultimate",
-        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-        githubLink: "https://hoidanit.vn/khoa-hoc/react-ultimate-react-co-ban-tu-z-toi-a-640b50b96cc592d780aab976.html",
-        demoLink: "https://hoidanit.vn/khoa-hoc/react-ultimate-react-co-ban-tu-z-toi-a-640b50b96cc592d780aab976.html"
-    }
-]
+  {
+    id: 1,
+    imgPath: nestImg,
+    title: "SoundCloud Clone | Next.js 14",
+    description:
+      "Dự án frontend nhân bản giao diện và các tính năng cốt lõi của SoundCloud. " +
+      "Xây dựng bằng Next.js 14, tận dụng các tính năng mới nhất như Server Actions để tối ưu hóa trải nghiệm người dùng. " +
+      "Tích hợp NextAuth để xử lý xác thực, và áp dụng các kỹ thuật caching dữ liệu nâng cao cùng với revalidateTag để đảm bảo dữ liệu luôn mới nhất và hiệu năng.",
+    githubLink: "https://github.com/your-username/soundcloud-clone", // Thay link của bạn vào đây
+    demoLink: "https://your-demo-link.com", // Thay link của bạn vào đây
+  },
+  {
+    id: 2,
+    imgPath: nestImg,
+    title: "E-commerce Bookstore | React.js",
+    description:
+      "Website thương mại điện tử chuyên bán sách, được xây dựng hoàn toàn bằng React. " +
+      "Giao diện người dùng được thiết kế chuyên nghiệp và thân thiện với thư viện Ant Design. " +
+      "Tích hợp các chức năng quan trọng như thanh toán online an toàn qua cổng VNPay và đăng nhập tiện lợi bằng tài khoản Google.",
+    githubLink: "https://github.com/your-username/react-bookstore", // Thay link của bạn vào đây
+    demoLink: "https://your-demo-link.com", // Thay link của bạn vào đây
+  },
+  {
+    id: 3,
+    imgPath: nestImg,
+    title: "Web Application | Java Spring MVC & JSP",
+    description:
+      "Một ứng dụng web được xây dựng theo kiến trúc Java Spring MVC truyền thống. " +
+      "Sử dụng JSP (JavaServer Pages) làm view engine để render giao diện phía server. " +
+      "Hệ thống xác thực người dùng dựa trên Session, tương tác với cơ sở dữ liệu thông qua JPA (Java Persistence API) và được bảo mật bởi Spring Security.",
+    githubLink: "https://github.com/your-username/spring-mvc-project", // Thay link của bạn vào đây
+    demoLink: "https://your-demo-link.com", // Thay link của bạn vào đây
+  },
+];
 
 export const SKILLS_DATA = [
-    'HTML',
-    'CSS',
-    'Javascript',
-    'Typescript',
-    'React',
-    'Next JS',
-    'Tailwind',
-    'MongoDB',
-    'MySQL',
-    'PostgreSQL',
-    'Git',
-    'AWS',
-    'Bootstrap',
-    'Docker',
-    'Go',
-    'Figma',
-    'Firebase',
-    'MaterialUI',
-    'Nginx',
-    'Strapi'
-  ]
-  
-  // Choose your skills from below. Make sure it's in the same format and spelled correctly.
-  // Couldn't find the required skills? Raise an issue on github at https://github.com/hhhrrrttt222111/developer-portfolio/issues/new
-  
-  
-  // AVAILABLE SKILLS
-  
-  /* 
+  "HTML",
+  "CSS",
+  "Javascript",
+  "Typescript",
+  "React",
+  "Next JS",
+  // 'Tailwind',
+  "MongoDB",
+  "MySQL",
+  "PostgreSQL",
+  "Git",
+  //   "AWS",
+  "Bootstrap",
+  "Docker",
+  //   "Go",
+  "Figma",
+  "Firebase",
+  "MaterialUI",
+  "Nginx",
+  "Java",
+  "Node JS",
+  //   "Strapi",
+];
+
+// Choose your skills from below. Make sure it's in the same format and spelled correctly.
+// Couldn't find the required skills? Raise an issue on github at https://github.com/hhhrrrttt222111/developer-portfolio/issues/new
+
+// AVAILABLE SKILLS
+
+/* 
     HTML
     CSS
     JS 
