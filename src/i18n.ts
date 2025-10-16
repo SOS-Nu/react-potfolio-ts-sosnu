f35767dc9543bca9fea9afd875f335d2ded7a9a9
nhokanhanh@gmail.com <nhokanhanh@gmail.com>
2025-10-16 18:42:59 +0700

    add languge default u18n

--- src/i18n.ts
+++ src/i18n.ts
@@ -17,10 +17,24 @@ i18n
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
     debug: false,
-    fallbackLng: "en",
-    lng: "en", // default language
+    fallbackLng: "vi",
+    // lng: "vi", // default language
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
+    },
+    detection: {
+      // Thứ tự i18next sẽ tìm ngôn ngữ
+      // 1. localStorage -> 2. cookie -> 3. Ngôn ngữ trình duyệt
+      order: [
+        "localStorage",
+        "cookie",
+        "navigator",
+        "htmlTag",
+        "path",
+        "subdomain",
+      ],
+      // Key để lưu trong localStorage
+      lookupLocalStorage: "i18nextLng",
     },
   });
 
